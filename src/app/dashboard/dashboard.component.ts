c7dcf6815616fc1c3b5043d070974f06aca97974
Gabriel Landim <55588010+Glandim@users.noreply.github.com>
2021-09-15 12:47:34 -0300

    dashboard 15/09

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -11,6 +11,20 @@ export class DashboardComponent implements OnInit {
     ) {
       this.images = [
         { name: 'http://lorempixel.com/640/480/animals/' },
+        { name: 'https://www.jasminealimentos.com/wp-content/uploads/2017/09/tipos-de-frutas-1-860x485.jpg' },
+        { name: 'http://lorempixel.com/640/480/business/' },
+        { name: 'http://lorempixel.com/640/480/cats/' },
+        { name: 'http://lorempixel.com/640/480/city/' },
+        { name: 'http://lorempixel.com/640/480/food/' },
+        { name: 'http://lorempixel.com/640/480/nightlife/' },
+        { name: 'http://lorempixel.com/640/480/fashion/' },
+        { name: 'http://lorempixel.com/640/480/people/' },
+        { name: 'http://lorempixel.com/640/480/nature/' },
+        { name: 'http://lorempixel.com/640/480/sports/' },
+        { name: 'http://lorempixel.com/640/480/transport/' },
+      ]
+      this.images2 = [
+        { name: 'http://lorempixel.com/640/480/animals/' },
         { name: 'http://lorempixel.com/640/480/abstract/' },
         { name: 'http://lorempixel.com/640/480/business/' },
         { name: 'http://lorempixel.com/640/480/cats/' },
@@ -27,4 +41,5 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {}
   images: Array<any> = []
+  images2: Array<any> = []
 }
