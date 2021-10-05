7804ea125163a6a1657db089b87014a7d87b2ca2
Gabriel Landim <55588010+Glandim@users.noreply.github.com>
2021-10-05 17:04:16 -0300

    Produto, dashboard 05/10

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import AuthCurrentTenant from 'src/app/auth/auth-current-tenant';
 import { AuthService } from 'src/app/auth/auth.service';
+import authAxios from 'src/app/shared/axios/auth-axios';
 import { i18n } from 'src/i18n';
 
 @Component({
@@ -7,39 +9,78 @@ import { i18n } from 'src/i18n';
   templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent implements OnInit {
-  constructor(private authService: AuthService,
-    ) {
-      this.images = [
-        { name: 'http://lorempixel.com/640/480/animals/' },
-        { name: 'https://www.jasminealimentos.com/wp-content/uploads/2017/09/tipos-de-frutas-1-860x485.jpg' },
-        { name: 'http://lorempixel.com/640/480/business/' },
-        { name: 'http://lorempixel.com/640/480/cats/' },
-        { name: 'http://lorempixel.com/640/480/city/' },
-        { name: 'http://lorempixel.com/640/480/food/' },
-        { name: 'http://lorempixel.com/640/480/nightlife/' },
-        { name: 'http://lorempixel.com/640/480/fashion/' },
-        { name: 'http://lorempixel.com/640/480/people/' },
-        { name: 'http://lorempixel.com/640/480/nature/' },
-        { name: 'http://lorempixel.com/640/480/sports/' },
-        { name: 'http://lorempixel.com/640/480/transport/' },
-      ]
-      this.images2 = [
-        { name: 'http://lorempixel.com/640/480/animals/' },
-        { name: 'http://lorempixel.com/640/480/abstract/' },
-        { name: 'http://lorempixel.com/640/480/business/' },
-        { name: 'http://lorempixel.com/640/480/cats/' },
-        { name: 'http://lorempixel.com/640/480/city/' },
-        { name: 'http://lorempixel.com/640/480/food/' },
-        { name: 'http://lorempixel.com/640/480/nightlife/' },
-        { name: 'http://lorempixel.com/640/480/fashion/' },
-        { name: 'http://lorempixel.com/640/480/people/' },
-        { name: 'http://lorempixel.com/640/480/nature/' },
-        { name: 'http://lorempixel.com/640/480/sports/' },
-        { name: 'http://lorempixel.com/640/480/transport/' },
-      ]
-    }
 
-  ngOnInit(): void {}
+  produto: any;
+
+  constructor(private authService: AuthService,
+  ) {
+    /* this.images = [
+      { name: 'http://lorempixel.com/640/480/animals/' },
+      { name: 'https://www.jasminealimentos.com/wp-content/uploads/2017/09/tipos-de-frutas-1-860x485.jpg' },
+      { name: 'http://lorempixel.com/640/480/business/' },
+      { name: 'http://lorempixel.com/640/480/cats/' },
+      { name: 'http://lorempixel.com/640/480/city/' },
+      { name: 'http://lorempixel.com/640/480/food/' },
+      { name: 'http://lorempixel.com/640/480/nightlife/' },
+      { name: 'http://lorempixel.com/640/480/fashion/' },
+      { name: 'http://lorempixel.com/640/480/people/' },
+      { name: 'http://lorempixel.com/640/480/nature/' },
+      { name: 'http://lorempixel.com/640/480/sports/' },
+      { name: 'http://lorempixel.com/640/480/transport/' },
+    ] */
+    this.images2 = [
+      /* { name: 'http://lorempixel.com/640/480/animals/' },
+      { name: 'http://lorempixel.com/640/480/abstract/' },
+      { name: 'http://lorempixel.com/640/480/business/' },
+      { name: 'http://lorempixel.com/640/480/cats/' },
+      { name: 'http://lorempixel.com/640/480/city/' },
+      { name: 'http://lorempixel.com/640/480/food/' },
+      { name: 'http://lorempixel.com/640/480/nightlife/' },
+      { name: 'http://lorempixel.com/640/480/fashion/' },
+      { name: 'http://lorempixel.com/640/480/people/' },
+      { name: 'http://lorempixel.com/640/480/nature/' },
+      { name: 'http://lorempixel.com/640/480/sports/' },
+      { name: 'http://lorempixel.com/640/480/transport/' }, */
+    ]
+  }
+
+  async ngOnInit() {
+    this.produto = await DashboardComponent.list()
+  }
   images: Array<any> = []
   images2: Array<any> = []
+
+  static async list() {
+
+    const tenantId = AuthCurrentTenant.get();
+
+    const response = await authAxios.get(
+      `/tenant/${tenantId}/produto?limit=15`,
+    );
+    
+    console.log(response.data.rows);
+    
+    return response.data.rows;
+  }
+  
+  verifyImage(image) {
+    if (image.length > 0) {
+      return image[0].downloadUrl
+    } else {
+      return 'assets/images/noImage.png'
+    }
+  }
+
+  transformValue(value) {
+    console.log(value);
+    
+    if (value != undefined) {
+      value = (value * 100) / 100
+
+      return (value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+      }))
+    }
+  }
 }
